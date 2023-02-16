5d6152ab3c7699e0212035d54467167ca5c08567
ciceroff <cicero.barrozo13@gmail.com>
2023-02-16 15:23:06 -0300

    ♻️ Finish sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import Post from './Post'
 import './global.css'
+import styles from './App.module.css'
+
 function App() {
   return (
     <div>
       <Header />
-      <Post author="Cicero Fernandes" content="Olá mundo" />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Cicero Fernandes" content="Olá mundo" />
+          <Post author="Gabriel Marques" content="Olá mundo" />
+        </main>
+      </div>
     </div>
   )
 }
