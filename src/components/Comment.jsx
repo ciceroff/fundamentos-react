7f7a56af6232e902cae128bb4d3270e0bed359cc
ciceroff <cicero.barrozo13@gmail.com>
2023-02-17 11:53:04 -0300

    🚧 add comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import style from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={style.comment}>
+      <img src="https://github.com/ciceroff.png" />
+      <div className={style.commentBox}>
+        <div className={style.commentContent}>
+          <header>
+            <div className={style.authorAndTime}>
+              <strong>Nome</strong>
+              <time title="11 de maio" dateTime="2023-02-17">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns</p>
+        </div>
+        <footer>
+          {' '}
+          <button>
+            {' '}
+            <ThumbsUp /> Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
